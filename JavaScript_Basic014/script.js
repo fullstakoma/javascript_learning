fa0d12ba472fbc2ffca1d77ef3f8345c0155b9e3
fullstakomatsu <fullstakomatsu@gmail.com>
2024-07-24 19:57:49 +0900

    微修正

--- JavaScript_Basic014/script.js
+++ JavaScript_Basic014/script.js
@@ -16,22 +16,48 @@ console.log(1, 2, 3, arraySample);
 console.log(1, 2, 3, ...arraySample);
 console.log(good_arraySample);
 
+//Spreadについて
+const arraySpread = [1, 2, 3, [4, 5, 6]];
+console.log(arraySpread);
+//RESTについて
+const [a, b, ...otherNumber] = [1, 2, 3, 4, 5, 6];
+console.log(a, b, otherNumber);
+
+const showNumbers = function (...numbers) {
+  console.log(numbers);
+};
+
+showNumbers(2, 3);
+showNumbers(1, 2, 3, 4, 5);
+
+const sumNumbers = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+const arrayToSum = [23, 5, 6];
+sumNumbers(...arrayToSum);
+
 //ラーメンショップを使った例
 const ramenShop = {
   shopName: "リアルラーメンマン",
   mainMenu: ["醤油ラーメン", "味噌ラーメン", "豚骨ラーメン"],
   sideMenu: ["餃子", "唐揚げ", "搾菜"],
   otsumami: [],
-  orderRamen: function (snack_1, snack_2, snack_3) {
+  orderOtsumami: function (snack_1, snack_2, snack_3) {
     console.log(
       `へい、おまちどう！おつまみセットだよ。本日は${snack_1}、${snack_2}、${snack_3}だよ`
     );
   },
+  orderRamen: function (main, ...toping) {
+    console.log(`へいおまちどう！${main}だよ。トッピングは${toping}`);
+  },
 };
 
 const snacklist = ["イカ塩", "チョリソー", "ゆで卵"];
-ramenShop.orderRamen(snacklist[0], snacklist[1], snacklist[2]);
-ramenShop.orderRamen(...snacklist);
+ramenShop.orderOtsumami(snacklist[0], snacklist[1], snacklist[2]);
+ramenShop.orderOtsumami(...snacklist);
 
 const newMainMenu = [...ramenShop.mainMenu, "塩ラーメン"];
 console.log(ramenShop.mainMenu);
@@ -52,3 +78,6 @@ console.log(addDataRamenShop);
 console.log(ramenShop.shopName);
 ramenShop.shopName = "復刻リアルラーメンマン";
 console.log(ramenShop.shopName);
+
+//RESTバージョン
+ramenShop.orderRamen("塩ラーメン", "ねぎ", "ゆで卵", "チャーシュー");
